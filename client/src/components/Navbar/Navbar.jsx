5711f7e7b3899abee128e611581d003a3852fa1e
Ajay <52690028+ajaykasturi@users.noreply.github.com>
2023-06-01 22:40:20 +0530

    Update Navbar.jsx

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
+import logo from '../../assets/logo.png';
+import search from '../../assets/searchicon.svg';
 const Navbar=()=>{
-    <div>
-        
-    </div>
+
+    return  (
+    <nav>
+        <div className="navbar">
+            <Link to='/' className='nav-item nav-btn'>
+                <img src={logo} alt="stackoverflow-logo" />
+            </Link>
+            <Link to='/' className='nav-item nav-btn'>About</Link>
+            <Link to='/' className='nav-item nav-btn'>Products</Link>
+            <Link to='/' className='nav-item nav-btn'>For Teams</Link>
+            <form>
+                <input type='text' placeholder='Search...' />
+                <img src={search} alt='search-icon' width=18/>
+            </form>
+        </div>
+    </nav>
+    );
 };
 
 export default Navbar;

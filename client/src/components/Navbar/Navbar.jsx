407aed708c854f77f3b9d814d91ff07aee080e0a
Ajay <52690028+ajaykasturi@users.noreply.github.com>
2023-06-02 23:33:12 +0530

    Update Navbar.jsx

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import logo from '../../assets/logo.png';
 import search from '../../assets/searchicon.svg';
+import Avatar from '../../components/Avatar/Avatar'
+import Button from '../../components/Button/Button'
+import './Navbar.css'
 const Navbar=()=>{
-
+    var User=null;
     return  (
     <nav>
         <div className="navbar">
-            <Link to='/' className='nav-item nav-btn'>
+            <Link to='/' className='nav-item nav-logo '>
                 <img src={logo} alt="stackoverflow-logo" />
             </Link>
             <Link to='/' className='nav-item nav-btn'>About</Link>
@@ -15,8 +18,16 @@ const Navbar=()=>{
             <Link to='/' className='nav-item nav-btn'>For Teams</Link>
             <form>
                 <input type='text' placeholder='Search...' />
-                <img src={search} alt='search-icon' width=18/>
+                <img src={search} alt='search-icon' width="18" className='search-icon' />
             </form>
+            {
+                User===null ? 
+                <Link to='/Auth' className='nav-item nav-links'>Log in</Link> :
+                <>
+                <Link to='/' className=''><Avatar>A</Avatar></Link> :
+                <Button >Log out</Button>
+                </>
+            }
         </div>
     </nav>
     );

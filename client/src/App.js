d466fe1a81d07bead6ebcff7a36c7575b39c345a
Ajay <52690028+ajaykasturi@users.noreply.github.com>
2023-06-04 21:03:58 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import AllRoutes from './AllRoutes';
 function App() {
   return (
     <div className='App'>
       <Router>
         <Navbar />
-      </Router>
+        <AllRoutes />
+      </Router> 
     </div>
   );
 }
